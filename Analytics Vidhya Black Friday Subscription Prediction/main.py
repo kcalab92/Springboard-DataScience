b5d193c3c5eea45849ee202271e4755d88ac5309
kcalab92 <kylecalabria92@gmail.com>
2018-05-11 16:59:22 -0400

    new name

---
+++ Analytics Vidhya Black Friday Subscription Prediction/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 18:37:25 2017
+
+@author: kylecalabria
+"""
+
+import pandas as pd
+
+test = pd.read_csv('test1.csv')
+
+print(test.head())
+
